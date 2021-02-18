fc981698e532dc196ba26bc934eea102199b1478
Pedro Coelho <79021161+pedrocoelho94@users.noreply.github.com>
2021-02-18 19:02:54 -0300

    :package: navbar/features/destaque/clientes

--- portfolio-pedro/js/script.js
+++ portfolio-pedro/js/script.js
@@ -1,5 +1,17 @@
-var hamburguer = document.querySelector(".hamburguer");
+jQuery(document).ready(function ($){
 
-hamburguer.addEventListener("click", function(){
-    document.querySelector(".container-teste").classList.toggle("show-menu");
-})
+    //FIXED HEADER
+    window.onscroll = function(){
+        if(window.pageYOffset > 150){
+            $("#menu").addClass("bg");
+            $("#header .collapse li a").addClass("cor-bg");
+
+            
+        }else{
+            $("#menu").removeClass("bg");
+            $("#header .collapse li a").removeClass("cor-bg"); 
+ 
+        }
+    };
+});
+
