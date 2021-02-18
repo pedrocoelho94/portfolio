ad0d904d6e0b65f57852590595ea3d6d30184e60
Pedro Coelho <79021161+pedrocoelho94@users.noreply.github.com>
2021-02-18 19:32:00 -0300

    :up: menu hamburguer
    menu hamburguer funcionando de forma correta

--- portfolio-pedro/js/script.js
+++ portfolio-pedro/js/script.js
@@ -5,12 +5,15 @@ jQuery(document).ready(function ($){
         if(window.pageYOffset > 150){
             $("#menu").addClass("bg");
             $("#header .collapse li a").addClass("cor-bg");
+            $(".navbar-toggler i").addClass("hamburguer-black");
+            $(".navbar-toggler i").removeClass("hamburguer");
 
             
         }else{
             $("#menu").removeClass("bg");
             $("#header .collapse li a").removeClass("cor-bg"); 
- 
+            $(".navbar-toggler i").removeClass("hamburguer-black");
+            $(".navbar-toggler i").addClass("hamburguer");
         }
     };
 });
